da8027500fa216fc61e25751b97738819db6311a
Kiet Nguyen <kietnguyen564042@gmail.com>
2022-07-04 09:11:31 +0700

    initial

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { engine } = require('express-handlebars');
+const morgan = require('morgan');
+const path = require('path');
+const app = express();
+const port = 3000;
+const route = require('./routes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
+
+//http
+app.use(morgan('combined'));
+
+//template engine
+app.engine(
+    'hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resource/views'));
+
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
